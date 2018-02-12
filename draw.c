01059a8ae35053c31109adf1eceb65d8e5b8660a
Rihui Zheng <rzheng4@stuy.edu>
2018-02-11 23:49:56 -0500

    nice pic

--- draw.c
+++ draw.c
@@ -7,26 +7,27 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-  int A = y1 - y0;
-  int B = x0 - x1;
-  
+   
   //if (x0,y0) is to the right of (x1,y1) then switsh
-  if (B > 0){
+  if (x0 > x1){
     int tmpx = x0;
     x0 = x1;
-    x1 = x0;
+    x1 = tmpx;
 
     int tmpy = y0;
     y0 = y1;
     y1 = tmpy;
+
   }
 
+  int A = y1 - y0;
+  int B = x0 - x1;
   int x = x0;
   int y = y0;
   float slope = (float)A/B * -1;
 
   //Octant 2                                                                                                                                                                                              
-  if (slope > 1){
+  if (slope >= 1){
     int d = A + 2 * B;
     while (y <= y1){
       plot (s, c, x, y);
@@ -41,7 +42,7 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
   
   
   //Octant 1
-  else if (slope > 0){
+  else if (slope >= 0 && slope < 1){
     int d = 2 * A + B;
     while (x <= x1){
       plot(s, c, x, y);
@@ -55,7 +56,7 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
   }
   
   //Octant 8
-  else if (slope > -1){
+  else if (slope >= -1 && slope < 0){
     int d = 2 * A - B;
     while (x <= x1){
       plot(s, c, x, y);
@@ -71,7 +72,7 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
   //Octant 7
   else{
     int d = A - 2 * B;
-    while (y <= y1){
+    while (y >= y1){
       plot(s, c, x, y);
       if (d > 0){
      	x++;
--- main.c
+++ main.c
@@ -17,9 +17,82 @@ int main() {
 
   clear_screen(s);
 
+  int i = 500;
+  
+  int x = 250;
+  int y = 0;
+
+  while (i >= 0){
+    if (y < 250 && x <= 250){
+      draw_line(0,0,x,y,s,c);
+      x -= 2;
+      y += 2;
+    }
+    else if (y >= 250 && x < 250){
+      draw_line(0,500,x,y,s,c);
+      x += 2;
+      y += 2;
+    }
+    else if (y > 250 && x >= 250){
+      draw_line(500,500,x,y,s,c);
+      x += 2;
+      y -= 2;
+    }
+    else{
+      draw_line(500,0,x,y,s,c);
+      x -= 2;
+      y -= 2;
+    }
+    
+    if (c.red < 255 && c.green > 0 && c.blue == 0){
+      c.red = c.red + 3;
+      c.green = c.green - 3;
+    }
+    else if(c.blue < 255 && c.red > 0 && c.green == 0){
+      c.blue = c.blue + 3;
+      c.red = c.red - 3;
+    }
+    else{
+      c.green = c.green + 3;
+      c.blue = c.blue - 3;
+    }
+    
+    i--;
+  }
+  
   
 
+    /*
+  //test
+  //Q1
+  draw_line(250,250,500,300,s,c);
+  draw_line(250,250,500,400,s,c);
+  draw_line(250,250,500,500,s,c);
+  draw_line(250,250,400,500,s,c);
+  draw_line(250,250,300,500,s,c);
+
+  //Q2
+  draw_line(250,250,0,300,s,c);
+  draw_line(250,250,0,400,s,c);
+  draw_line(250,250,0,500,s,c);
+  draw_line(250,250,100,500,s,c);
+  draw_line(250,250,200,500,s,c);
+
+  //Q3
+  draw_line(250,250,0,200,s,c);
+  draw_line(250,250,0,100,s,c);
+  draw_line(250,250,0,0,s,c);
+  draw_line(250,250,100,0,s,c);
+  draw_line(250,250,200,0,s,c);
   
+  //Q4
+  draw_line(250,250,500,200,s,c);
+  draw_line(250,250,500,100,s,c);
+  draw_line(250,250,500,0,s,c);
+  draw_line(250,250,400,0,s,c);
+  draw_line(250,250,300,0,s,c);
+  */  
+
   display(s);
   save_extension(s, "lines.png");
 }

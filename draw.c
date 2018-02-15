5ba7c1d8dd5ceb1cf440172f044b4d3f451d0c1a
Rihui Zheng <rzheng4@stuy.edu>
2018-02-15 13:48:35 -0500

    doon

--- draw.c
+++ draw.c
@@ -5,58 +5,57 @@
 #include "display.h"
 #include "draw.h"
 
-//Insert your line algorithm here
+
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-   
-  //if (x0,y0) is to the right of (x1,y1) then switsh
-  if (x0 > x1){
-    int tmpx = x0;
-    x0 = x1;
-    x1 = tmpx;
-
-    int tmpy = y0;
-    y0 = y1;
-    y1 = tmpy;
-
-  }
-
-  int A = y1 - y0;
-  int B = x0 - x1;
+  
   int x = x0;
   int y = y0;
-  float  slope = (float)A/B * -1;
+  
+  //if (x0,y0) is to the right of (x1,y1) then switch
+  if (x0 > x1){
+    x = x1;
+    x1 = x0;
+    
+    y = y1;
+    y1 = y0;
+    
+  }
+  
+  int A = y1 - y;
+  int B = x - x1;
+  
+  //float  slope = (float)A/B * -1;
   
   //Octant 2                                                                                                                                                                                              
-  if (slope >= 1 || !B){
+  if (A > 0 && A >= -B){//slope >= 1 || (!B && y1 > y0)){
     int d = A + 2 * B;
     while (y <= y1){
       plot (s, c, x, y);
       if (d < 0){
 	x++;
 	d += 2 * A;
-      }
+    }
       y++;
       d += 2 * B;
     }
   }
-  
-  
+
   //Octant 1
-  else if (slope >= 0 && slope < 1){
+  else if (A >= 0 && A < -B){//slope >= 0 && slope < 1 && B){
     int d = 2 * A + B;
     while (x <= x1){
       plot(s, c, x, y);
       if (d > 0){
 	y++;
 	d += 2 * B;
-      }
+     }
       x++;
       d += 2 * A;
     }
   }
   
   //Octant 8
-  else if (slope >= -1 && slope < 0){
+  else if (A < 0 && A >= B){//slope >= -1 && slope < 0 && B){
     int d = 2 * A - B;
     while (x <= x1){
       plot(s, c, x, y);
